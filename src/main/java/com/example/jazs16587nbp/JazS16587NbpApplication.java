83a4e962008c1bcb8529a8030d129b4201d490c9
kacperkaczor97 <kacperkaczor97@gmail.com>
2021-07-03 13:43:36 +0200

    initial commit

---
+++ src/main/java/com/example/jazs16587nbp/JazS16587NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs16587nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS16587NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS16587NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jazs16587nbp/JazS16587NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs16587nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS16587NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
